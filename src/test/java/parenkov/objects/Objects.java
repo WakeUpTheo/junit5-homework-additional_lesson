9d6917f65e6912e8d4e056b60bbd8298b7d2c71c
ParenkovLocal <parenkov.fedor@gmail.com>
2021-10-27 15:47:57 +0400

    Edited page objects

--- src/test/java/parenkov/objects/Objects.java
+++ src/test/java/parenkov/objects/Objects.java
@@ -1,21 +1,25 @@
 package parenkov.objects;
 
+import com.codeborne.selenide.SelenideElement;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class Objects {
 
+    private SelenideElement searchField = $("[name=q]");
+    private SelenideElement result = $(".eqAnXb h3");
+
     public void openGoogle() {
         open("https://www.google.com/");
     }
 
     public void doSearch(String value) {
-        $("[name=q]").setValue(value).pressEnter();
+        searchField.setValue(value).pressEnter();
     }
 
     public void checkResults(String value) {
-        $(".eqAnXb h3").shouldHave(text(value));
+        result.shouldHave(text(value));
     }
-
 }
